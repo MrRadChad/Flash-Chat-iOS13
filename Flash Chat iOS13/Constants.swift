cba2f25c3b3ac6bfa4126380c07882d1d2fc2e98
Chad McKinney <cfmckinney@gmail.com>
2020-03-03 12:39:21 -0700

    Added one additional user support.

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  Swift Chat
+//
+//  Created by James Frazee on 2/27/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+struct Constants {
+    
+    static let appName = "⚡️SwiftChat"
+    
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
